a28e60b0ff8973fac72f607ade4f129cd5ea1168
squ1dggly <fluffyspiderstudios@gmail.com>
2026-02-14 15:51:32 -1000

    added rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async rewrites() {
+        return [
+            {
+                has: [{ type: "header", key: "x-forwarded-host", value: "concepts.guniqueg.com" }],
+                source: "/business-card",
+                destination: "guniqueg-concept-business-card-52s0u3zvc-xsqu1znts-projects.vercel.app"
+            },
+            {
+                has: [{ type: "header", key: "x-forwarded-host", value: "concepts.guniqueg.com" }],
+                source: "/avatar-list",
+                destination: "snippet-fluid-morphing-avatar-list-gqbyfjnja-xsqu1znts-projects.vercel.app"
+            }
+        ];
+    }
 };
 
 export default nextConfig;
