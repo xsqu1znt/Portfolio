bcd3717bc9f82b20c2317226066f13153a3784c0
squ1dggly <fluffyspiderstudios@gmail.com>
2026-02-14 15:58:07 -1000

    config

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,28 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     async rewrites() {
         return [
+            // Business Card Concept
             {
                 has: [{ type: "header", key: "x-forwarded-host", value: "concepts.guniqueg.com" }],
                 source: "/business-card",
-                destination: "guniqueg-concept-business-card-52s0u3zvc-xsqu1znts-projects.vercel.app"
+                destination: "guniqueg-concept-business-card.vercel.app"
             },
             {
                 has: [{ type: "header", key: "x-forwarded-host", value: "concepts.guniqueg.com" }],
+                source: "/business-card/:path*",
+                destination: "guniqueg-concept-business-card.vercel.app/:path*"
+            },
+
+            // Avatar List Concept
+            {
+                has: [{ type: "header", key: "x-forwarded-host", value: "concepts.guniqueg.com" }],
                 source: "/avatar-list",
-                destination: "snippet-fluid-morphing-avatar-list-gqbyfjnja-xsqu1znts-projects.vercel.app"
+                destination: "guniqueg-concept-avatar-list.vercel.app"
+            },
+            {
+                has: [{ type: "header", key: "x-forwarded-host", value: "concepts.guniqueg.com" }],
+                source: "/avatar-list/:path*",
+                destination: "guniqueg-concept-avatar-list.vercel.app/:path*"
             }
         ];
     }
